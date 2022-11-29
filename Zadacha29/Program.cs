1fb1b2819f5208edc3c7a6343916cdd57b324011
Julia <julso@LAPTOP-J5CQF40T.(none)>
2022-11-29 16:20:13 +0300

    Zadacha 29 added

---
+++ Zadacha29/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая задаёт массив из N элементов 
+//и выводит их на экран. Вывод сделать отдельным методом.
+//5 -> [1, 2, 5, 7, 19]
+//3 -> [6, 1, 33]
+
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int [] numbers = new int [size];
+
+for(int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = new Random().Next(0, 10);
+}
+
+WriteArray(numbers);
+
+void WriteArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+    Console.Write(array[i] + " ");
+    }
+}
