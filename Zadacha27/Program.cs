ced9ac19575174b97e2355905c519c535411f15d
Julia <julso@LAPTOP-J5CQF40T.(none)>
2022-11-29 16:00:29 +0300

    Zadacha 27 added

---
+++ Zadacha27/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+string stringNumber = Console.ReadLine();
+
+int sum = 0;
+
+for (int i = 0; i < stringNumber.Length; i++)
+{
+    string digit = Convert.ToString(stringNumber[i]);
+    sum += Convert.ToInt32(digit);
+}
+
+Console.WriteLine("Сумма цифр в числе = "+ sum);
